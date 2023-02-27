de8cfcf58424390fad6c4ed3987f4abe0cfaaee5
Catherine Roberts <crobert7@email.unc.edu>
2023-02-26 21:41:51 -0500

    adding scrolling, read more button, etc

--- cath-website/src/app/projects/projects.component.ts
+++ cath-website/src/app/projects/projects.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
+  
+  readMore(): void {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
 
+    if (dots !== null && btnText !== null && moreText !== null) {
+      console.log("No null values")
+      if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+      } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+      }
+    }
+  }
 }
